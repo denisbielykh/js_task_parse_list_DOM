49d8ad972bb55bdbc4fd542af7c3f47249183722
denys <denis.bielykh38@gmail.com>
2025-01-21 22:07:45 +0200

    sulution-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+const employees = [...document.querySelectorAll('li')];
+const employeesFixedSalary = parseSalary(employees);
+const employeesList = document.querySelector('ul');
+const sortedEmployeesBySalary = sortEmployeesBySalary(employeesFixedSalary);
+
+updateEmployeesList(sortedEmployeesBySalary);
+
+function sortEmployeesBySalary(employees) {
+  return employees.sort((a, b) => {
+    return a.dataset.salary - b.dataset.salary;
+  });
+}
+
+function parseSalary(employees) {
+  const newEmployees = [...employees];
+
+  for (const employee of employees) {
+    const newDataSalaryValue = employee.dataset.salary
+      .replace('$', '')
+      .replace(',', '');
+
+    employee.dataset.salary = newDataSalaryValue;
+  }
+
+  return newEmployees;
+}
+
+function updateEmployeesList(employees) {
+  employeesList.textContent = '';
+
+  for (const employee of employees) {
+    employeesList.append(employee);
+  }
+}
